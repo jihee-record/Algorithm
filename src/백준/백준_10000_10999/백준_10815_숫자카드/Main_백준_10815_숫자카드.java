eac626d9fd36fdb10951cccf6d9daa32401cd3a1
SOM2 <som2plestory@naver.com>
2024-12-17 14:38:27 +0900

    Create Main_백준_10815_숫자카드.java

---
+++ src/백준/백준_10000_10999/백준_10815_숫자카드/Main_백준_10815_숫자카드.java
@@ -0,0 +1,44 @@
+package 백준.백준_10000_10999.백준_10815_숫자카드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main_백준_10815_숫자카드 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Set<Integer> cards = new HashSet<>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int n=0; n<N; n++) {
+			cards.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int m=0; m<M; m++) {
+			if(cards.contains(Integer.parseInt(st.nextToken()))) {
+				sb.append("1 ");
+			}else {
+				sb.append("0 ");
+			}
+		}
+		
+		sb.setLength(sb.length() - 1);
+		System.out.println(sb);
+		br.close();
+	}
+
+}
