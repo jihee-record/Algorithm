70d0b8fe8d4e6e01e96e12efabf7455729b34681
SOM2 <som2plestory@naver.com>
2025-01-26 23:19:59 +0900

    Create Solution_프로그래머스_코테연습_징검다리건너기_LV3.java

---
+++ src/프로그래머스/프로그래머스_코테연습/프로그래머스_코테연습_징검다리건너기_LV3/Solution_프로그래머스_코테연습_징검다리건너기_LV3.java
@@ -0,0 +1,36 @@
+package 프로그래머스.프로그래머스_코테연습.프로그래머스_코테연습_징검다리건너기_LV3;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution_프로그래머스_코테연습_징검다리건너기_LV3 {
+
+	public int solution(int[] stones, int k) {
+        int answer = Integer.MAX_VALUE;
+        // index 모음
+        Deque<Integer> dq = new LinkedList<>();
+        
+        for(int i=0; i<stones.length; i++) {
+        	// 구간 개수 이전꺼 제외
+        	if(!dq.isEmpty() && dq.peekFirst() < i - k +1) {
+        		dq.pollFirst();
+        	}
+        	
+        	// 구간에서 작은 애들 지움
+        	while(!dq.isEmpty() && stones[dq.peekLast()] <= stones[i]) {
+        		dq.pollLast();
+        	}
+        	
+        	dq.offerLast(i);
+        	
+        	if(i >= k-1) {
+        		// 구간 최댓값 중에서 작은 값
+        		answer = Math.min(answer, stones[dq.peekFirst()]);
+        		System.out.println("i: " + i + ", answer: " + answer);
+        	}
+        }
+        
+        return answer;
+    }
+
+}
