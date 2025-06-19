13ea8cd864e05b67fa8eedadc23adeab365e33d5
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2025-06-19 22:00:55 +0900

    move

--- src/백준/백준_7000_7999/백준_7785_회사에있는사람_S5/Main_백준_7785_회사에있는사람_S5_552ms.java
+++ src/백준/백준_7000_7999/백준_7785_회사에있는사람_S5/Main_백준_7785_회사에있는사람_S5_552ms.java
@@ -3,11 +3,11 @@ package 백준.백준_7000_7999.백준_7785_회사에있는사람_S5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Set;
+import java.util.NavigableSet;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
-public class Main_백준_7785_회사에있는사람_S5 {
+public class Main_백준_7785_회사에있는사람_S5_552ms {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +15,7 @@ public class Main_백준_7785_회사에있는사람_S5 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		Set<String> set = new TreeSet<>();
+		NavigableSet<String> set = new TreeSet<>();
 		
 		for(int n=0; n<N; n++) {
 			st = new StringTokenizer(br.readLine());
@@ -28,11 +28,11 @@ public class Main_백준_7785_회사에있는사람_S5 {
 		}
 		
 		StringBuilder sb = new StringBuilder();
+		while(!set.isEmpty()) {
+			sb.append(set.pollLast()).append("\n");
+		}
 		
-		
-		
-		
-		
+		System.out.println(sb.toString());
 		br.close();
 	}
 }
