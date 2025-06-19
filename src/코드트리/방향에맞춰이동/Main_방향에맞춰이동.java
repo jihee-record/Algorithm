c74800e687262a8162c08f295802add72e65cca8
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2025-06-19 22:00:37 +0900

    Create Main_방향에맞춰이동.java

---
+++ src/코드트리/방향에맞춰이동/Main_방향에맞춰이동.java
@@ -0,0 +1,34 @@
+package 코드트리.방향에맞춰이동;
+
+import java.util.Scanner;
+
+public class Main_방향에맞춰이동 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int dx=0, dy=0;
+        
+        for (int i = 0; i < n; i++) {
+            char direction = sc.next().charAt(0);
+            int distance = sc.nextInt();
+            
+            switch(direction) {
+            case 'N':
+            	dy = dy + distance;
+            	break;
+            case 'E':
+            	dx = dx + distance;
+            	break;
+            case 'W':
+            	dx = dx - distance;
+            	break;
+            case 'S':
+            	dy = dy - distance;
+            }
+        }
+        
+        System.out.println(dx + " " + dy);
+        sc.close();
+    }
+}
