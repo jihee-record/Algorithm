75b31aec937423e9e67acd409bd2bb53a4b4fd41
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2025-07-24 22:01:11 +0900

    Create Main_백준_11053_가장긴증가하는부분수열_S2_112ms.java

---
+++ src/백준/백준_10000_10999/백준_11053_가장긴증가하는부분수열_S2/Main_백준_11053_가장긴증가하는부분수열_S2_112ms.java
@@ -0,0 +1,45 @@
+package 백준.백준_10000_10999.백준_11053_가장긴증가하는부분수열_S2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_백준_11053_가장긴증가하는부분수열_S2_112ms {
+	
+	static int search(List<Integer> lis, int now) {
+		int left = 0;
+		int right = lis.size() - 1;
+		
+		while(left <= right) {
+			int mid = (left + right) / 2;
+			
+			if(lis.get(mid) < now) left = mid + 1;
+			else right = mid - 1;
+		}
+		
+		return left;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		List<Integer> lis = new ArrayList<>();
+		for(int n=0; n<N; n++) {
+			int now = Integer.parseInt(st.nextToken());
+			int idx = search(lis, now);
+			
+			if(idx < lis.size()) lis.set(idx, now);
+			else lis.add(now);
+		}
+		
+		System.out.println(lis.size());
+		br.close();
+	}
+
+}
