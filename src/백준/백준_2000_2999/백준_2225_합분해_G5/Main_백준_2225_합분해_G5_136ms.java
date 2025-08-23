9e91bb9e64335f7f7067ef80b0dbfb84c010eb59
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2025-08-23 22:00:31 +0900

    Create Main_백준_2225_합분해_G5_136ms.java

---
+++ src/백준/백준_2000_2999/백준_2225_합분해_G5/Main_백준_2225_합분해_G5_136ms.java
@@ -0,0 +1,32 @@
+package 백준.백준_2000_2999.백준_2225_합분해_G5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_2225_합분해_G5_136ms {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		long[][] dp = new long[N+1][K+1];
+		for(int k=1; k<=K; k++) dp[0][k] = 1;
+		for(int n=1; n<=N; n++) dp[n][1] = 1;
+		
+		for(int n=1; n<=N; n++) {
+			for(int k=2; k<=K; k++) {
+				long sum = 0;
+				for(int i=0; i<=n; i++) sum += dp[i][k-1];
+				dp[n][k] = sum % 1000000000;
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+		br.close();
+	}
+
+}
