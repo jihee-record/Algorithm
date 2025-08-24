39d81b9e7b582b83ab20df8e78de028940182ed4
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2025-08-24 22:00:49 +0900

    Update Main_코드트리_방향에맞춰이동.java

--- src/코드트리/코드트리_방향에맞춰이동/Main_코드트리_방향에맞춰이동.java
+++ src/코드트리/코드트리_방향에맞춰이동/Main_코드트리_방향에맞춰이동.java
@@ -1,8 +1,8 @@
-package 코드트리.방향에맞춰이동;
+package 코드트리.코드트리_방향에맞춰이동;
 
 import java.util.Scanner;
 
-public class Main_방향에맞춰이동 {
+public class Main_코드트리_방향에맞춰이동 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
