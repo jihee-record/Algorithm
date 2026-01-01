2bbffff2489544de22ee36a078db834850941dae
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-01-01 20:53:46 +0900

    Create Main_백준_2263_트리의순회_G1_404ms.java

---
+++ src/백준/백준_2000_2999/백준_2263_트리의순회_G1/Main_백준_2263_트리의순회_G1_404ms.java
@@ -0,0 +1,53 @@
+package 백준.백준_2000_2999.백준_2263_트리의순회_G1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 트리 역추적 - dfs - 재귀
+public class Main_백준_2263_트리의순회_G1_404ms {
+	static StringBuilder sb;
+	static int[] inorder, post;
+	static int[] inorderIdx;
+	
+	static final void findPre(int postL, int postR, int inL, int inR) {
+		if(postL > postR) return;
+		int postRoot = post[postR];
+		sb.append(postRoot).append(" ");
+		
+		int rootIdx = inorderIdx[postRoot];
+		int sizeL = rootIdx - inL;
+//		int sizeR = inR - rootIdx; // 왼쪽트리 개수가 정해지면 오른쪽트리의 노드개수도 정해짐
+		
+		findPre(postL, postL + sizeL - 1, inL, rootIdx - 1);
+		findPre(postL + sizeL, postR - 1, rootIdx + 1, inR);
+	}
+
+	// 임의의 숫작 아니라 1~N 숫자로 지정
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		
+		inorder = new int[N];
+		inorderIdx = new int[N+1];
+		post = new int[N];
+		
+		for(int n=0; n<N; n++) {
+			int inorderVal = Integer.parseInt(st1.nextToken());
+			inorder[n] = inorderVal;
+			inorderIdx[inorderVal] = n;
+			post[n] = Integer.parseInt(st2.nextToken());
+		}
+
+		findPre(0, N-1, 0, N-1);
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+}
