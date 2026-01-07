3e9aa546f9c57ead9c5781057a9f71e1b170b7ad
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-01-07 20:55:33 +0900

    Create Main.java

---
+++ src/백준/백준_29000_29999/백준_29759_서로소스도쿠_G4/Main.java
@@ -0,0 +1,17 @@
+package 백준.백준_29000_29999.백준_29759_서로소스도쿠_G4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int[] prime = {2, 3, 5, 7, 11, 13, 17, 19,23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}; //25개
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		br.close();
+	}
+	
+}
