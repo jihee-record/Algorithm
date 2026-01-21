e2f4acd3aa2950e42ede8d2d261e7f27d4cb4bfe
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-01-21 20:59:47 +0900

    Create Main_백준_11444_피보나치수_G2.java

---
+++ src/백준/백준_11000_11999/백준_11444_피보나치수_G2/Main_백준_11444_피보나치수_G2.java
@@ -0,0 +1,20 @@
+//package 백준.백준_11000_11999.백준_11444_피보나치수_G2;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Main_백준_11444_피보나치수_G2 {
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int mod = 1_000_000_007;
+//		long N = Long.parseLong(br.readLine());
+//		
+//		int a = 0, b = 1, cycle = 0;
+//		
+//		System.out.println(a);
+//		br.close();
+//	}
+//}
