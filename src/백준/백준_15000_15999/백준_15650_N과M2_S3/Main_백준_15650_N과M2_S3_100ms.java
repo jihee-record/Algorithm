d9245383eab7a300768873c4bee64f427c14fc58
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-01-27 20:34:23 +0900

    Create Main_백준_15650_N과M2_S3_100ms.java

---
+++ src/백준/백준_15000_15999/백준_15650_N과M2_S3/Main_백준_15650_N과M2_S3_100ms.java
@@ -0,0 +1,43 @@
+package 백준.백준_15000_15999.백준_15650_N과M2_S3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_15650_N과M2_S3_100ms {
+	static StringBuilder sb;
+	static int N, M;
+	static int[] out;
+	
+	static void dfs(int n, int depth) {
+		if(depth == M) {
+			for(int c : out) sb.append(c).append(" ");
+			sb.append("\n");
+			return;
+		}
+
+		if(n > N) return;
+		
+		out[depth] = n;
+		dfs(n+1, depth+1);
+		dfs(n+1, depth);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		out = new int[M];
+		dfs(1, 0);
+		
+		System.out.println(sb);
+		br.close();
+	}
+
+}
