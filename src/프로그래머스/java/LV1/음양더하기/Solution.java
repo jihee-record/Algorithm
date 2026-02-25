6a1762a73b63911810bbcd6d40d0a07bde503160
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-02-25 22:19:29 +0900

    Create Solution.java

---
+++ src/프로그래머스/java/LV1/음양더하기/Solution.java
@@ -0,0 +1,18 @@
+package 프로그래머스.java.LV1.음양더하기;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i]){
+                answer += absolutes[i];
+            }
+            else{
+                answer -= absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}
