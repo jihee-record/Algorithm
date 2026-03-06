047f355e199c424bc559fd8d64d652822d21ae46
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-03-06 22:23:45 +0900

    Create Solution.java

---
+++ src/프로그래머스/java/LV1/핸드폰번호가리기/Solution.java
@@ -0,0 +1,12 @@
+package 프로그래머스.java.LV1.핸드폰번호가리기;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i=0; i<phone_number.length()-4; i++){
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length()-4, phone_number.length());
+        return answer;
+    }
+}
