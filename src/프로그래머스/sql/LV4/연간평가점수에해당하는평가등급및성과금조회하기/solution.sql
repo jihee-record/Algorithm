07bab88e76cbb70c5d658fd881035ec57c734e4b
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-03-16 22:27:26 +0900

    Create solution.sql

---
+++ src/프로그래머스/sql/LV4/연간평가점수에해당하는평가등급및성과금조회하기/solution.sql
@@ -0,0 +1,26 @@
+-- mysql
+SELECT 
+    E.EMP_NO,
+    E.EMP_NAME,
+    G.GRADE,
+    CASE
+        WHEN G.GRADE = 'S' THEN E.SAL * 0.2
+        WHEN G.GRADE = 'A' THEN E.SAL * 0.15
+        WHEN G.GRADE = 'B' THEN E.SAL * 0.1
+        ELSE 0
+    END BONUS
+FROM HR_EMPLOYEES E
+JOIN (
+    SELECT
+        EMP_NO,
+        CASE 
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C'
+        END GRADE
+    FROM HR_GRADE 
+    GROUP BY EMP_NO
+) G
+ON E.EMP_NO = G.EMP_NO
+ORDER BY E.EMP_NO
