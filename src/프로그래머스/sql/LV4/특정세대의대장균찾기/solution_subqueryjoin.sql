60bab86099d29a4df73542cd59bbc5be9a4ea6dd
이지희 <133189092+SOM2-RECORD@users.noreply.github.com>
2026-03-27 22:29:19 +0900

    Create solution_subqueryjoin.sql

---
+++ src/프로그래머스/sql/LV4/특정세대의대장균찾기/solution_subqueryjoin.sql
@@ -0,0 +1,28 @@
+--mysql
+/**
+3 >= 2 > 1 순서로 효율
+
+1. 재귀 방식으로 구하는 데, 3세대까지 밖에 안하고 3세대만 구하는 거라서 굳이 필요하지 않음.
+2. JOIN에 SUBQUERY를 써서 테이블 하나를 더 생성함 - 중간 결과를 따로 뺄 필요가 있나? 아니라서 불필요
+3. 그냥 FLAT 하게 조인을 나열 함
+*/
+SELECT 
+    D3.ID
+FROM ECOLI_DATA D3
+JOIN (
+    SELECT
+        D.ID,
+        GENERATION + 1 AS GENERATION
+    FROM ECOLI_DATA D
+    JOIN (
+            SELECT 
+                ID,
+                1 AS GENERATION
+            FROM ECOLI_DATA
+            WHERE PARENT_ID IS NULL
+        ) D1
+    ON D.PARENT_ID = D1.ID
+) D2
+ON D3.PARENT_ID = D2.ID
+ORDER BY D3.ID
+
